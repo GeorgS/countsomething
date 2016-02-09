4df8670fa51385b8d162d900caebeefcc4b1cd4e
Georg Schelkshorn <georg.schelkshorn@touchlay.com>
2016-02-09 08:34:22 +0100

    check if counter exists when exporting

--- src/utils/export.js
+++ src/utils/export.js
@@ -14,33 +14,35 @@ export const exportCSV = function exportCSV(props) {
   str += locationsStr;
   str += '; ; ;' + locationsStr + '\r\n';
   for (var i = 1; i < Object.keys(array).length+1; i++) {
-    var line = new Array();
-    line.push('"' + array[i].name + '"');
-    var total = 0;
-    if (array[i].hasStore) {
-    Object.keys(locations).forEach(function(key, index) {
-      var status = ((array[i].total[key] || 0) + (array[i].hasStore ? ((array[i].counts[today()] || 0)[key] || 0) : ((array[i].counts[today()] || 0).noStore || 0)));
-      line.push('"' + status + '"');
-      total += status;
-    });
-   } else {
-      line.push('"0"');
-      line.push('"0"');
+    if (array.hasOwnProperty(i)) {
+      var line = new Array();
+      line.push('"' + array[i].name + '"');
+      var total = 0;
+      if (array[i].hasStore) {
+      Object.keys(locations).forEach(function(key, index) {
+        var status = ((array[i].total[key] || 0) + (array[i].hasStore ? ((array[i].counts[today()] || 0)[key] || 0) : ((array[i].counts[today()] || 0).noStore || 0)));
+        line.push('"' + status + '"');
+        total += status;
+      });
+     } else {
+        line.push('"0"');
+        line.push('"0"');
+      }
+      line.push('"' + (total || 0) + '"');
+      line.push('"' + (array[i].hasStore ? ((array[i].counts[today()] || 0)[1] || 0) : ((array[i].counts[today()] || 0).noStore || 0)) + '"');
+      line.push(array[i].price.toString().replace('.', ',') + ' €');
+      var sumEl = (parseFloat(array[i].price) * (array[i].hasStore ? ((array[i].counts[today()] || 0)[1] || 0) : ((array[i].counts[today()] || 0).noStore || 0))).toFixed(2);
+      line.push('"' + sumEl.replace('.', ',') + ' €"');
+      totalSum += parseFloat(sumEl);
+      var total2 = 0;
+      Object.keys(locations).forEach(function(key, index) {
+        line.push('"' + (array[i].total[key] || 0) + '"');
+        total2 += (array[i].total[key] || 0);
+      });
+      line.push('"' + (total2 || 0) + '"');
+      str += line.join(';');
+      str += '\r\n';
     }
-    line.push('"' + (total || 0) + '"');
-    line.push('"' + (array[i].hasStore ? ((array[i].counts[today()] || 0)[1] || 0) : ((array[i].counts[today()] || 0).noStore || 0)) + '"');
-    line.push(array[i].price.toString().replace('.', ',') + ' €');
-    var sumEl = (parseFloat(array[i].price) * (array[i].hasStore ? ((array[i].counts[today()] || 0)[1] || 0) : ((array[i].counts[today()] || 0).noStore || 0))).toFixed(2);
-    line.push('"' + sumEl.replace('.', ',') + ' €"');
-    totalSum += parseFloat(sumEl);
-    var total2 = 0;
-    Object.keys(locations).forEach(function(key, index) {
-      line.push('"' + (array[i].total[key] || 0) + '"');
-      total2 += (array[i].total[key] || 0);
-    });
-    line.push('"' + (total2 || 0) + '"');
-    str += line.join(';');
-    str += '\r\n';
   }
   str += '"Gesamt";;;;;;' + totalSum.toFixed(2).replace('.', ',') + ' €;;;;\r\n';
   var link = document.createElement('a');
